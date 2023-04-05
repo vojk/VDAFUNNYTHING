3367e3f40bc8dcbb6cec4f72d31b601b8089ca4a
Vojtěch Tmej <37409766+vojk@users.noreply.github.com>
2023-04-05 10:20:58 +0200

    Add files via upload

---
+++ list.js
@@ -0,0 +1,7 @@
+const texts = ['Nastala chyba u Windows aplikace <br> <span class="addtional_text_level_one">není to tvůj problém ale Microsoftu</span>',
+    'Systém Windows se aktualizuje <br> <span class="addtional_text_level_one">tato akce může trvat několik let</span>',
+    'Chtěli jste si uložit soubor? <br> <span class="addtional_text_level_one">windows aktualizace řekla ne</span>',
+    'V klidu si můžete udělat kafe <br> <span class="addtional_text_level_one">ve finále i zásobu na celý život</span>',
+    'Hmm interesting práce <br> <span class="addtional_text_level_one">uloženo? ne? to mě (ne)mrzí</span>',
+    'My vás sledujeme pomocí Edge <br> <span class="addtional_text_level_one">nemusíte se bát, nedostane se to nikam</span> <br> <span class="addtional_text_level_two">Všechny data posíláme do všech zemí 3. světa...</span>'
+];
