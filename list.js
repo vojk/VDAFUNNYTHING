6692bbcf862dc5946b3a67ef6371f01589546d68
Vojtěch Tmej <37409766+vojk@users.noreply.github.com>
2023-04-13 13:11:55 +0200

    Update list.js

--- list.js
+++ list.js
@@ -3,5 +3,6 @@ const texts = ['Nastala chyba u Windows aplikace <br> <span class="addtional_tex
     'Chtěli jste si uložit soubor? <br> <span class="addtional_text_level_one">windows aktualizace řekla ne</span>',
     'V klidu si můžete udělat kafe <br> <span class="addtional_text_level_one">ve finále i zásobu na celý život</span>',
     'Hmm interesting práce <br> <span class="addtional_text_level_one">uloženo? ne? to mě (ne)mrzí</span>',
-    'My vás sledujeme pomocí Edge <br> <span class="addtional_text_level_one">nemusíte se bát, nedostane se to nikam</span> <br> <span class="addtional_text_level_two">Všechny data posíláme do všech zemí 3. světa...</span>'
+    'My vás sledujeme pomocí Edge <br> <span class="addtional_text_level_one">nemusíte se bát, nedostane se to nikam</span> <br> <span class="addtional_text_level_two">Všechny data posíláme do všech zemí 3. světa...</span>',
+    'Tento text asi nevidíte <br> <span class="addtional_text_level_one">Jelikož jsme ukradli Váš monitor</span>',
 ];
